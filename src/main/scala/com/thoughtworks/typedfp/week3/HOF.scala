4dd90df707b07cdbfa52d59c483e6a37375ce91b
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-04-06 19:04:42 +0530

    Implement solutions for HOF.scala

--- src/main/scala/com/thoughtworks/typedfp/week3/HOF.scala
+++ src/main/scala/com/thoughtworks/typedfp/week3/HOF.scala
@@ -11,23 +11,23 @@ object HOF extends App {
 
   type EmailFilter = Email => Boolean
 
-  def newMailsForUser(mails: Seq[Email], f: EmailFilter): Seq[Email] = ???
+  def newMailsForUser(mails: Seq[Email], f: EmailFilter): Seq[Email] = mails filter f
 
-  val sentByOneOf: Set[String] => EmailFilter = senders => email => ???
-  val notSentByAnyOf: Set[String] => EmailFilter = senders => email => ???
+  val sentByOneOf: Set[String] => Email => Boolean = senders => email => senders.contains(email.sender)
+  val notSentByAnyOf: Set[String] => EmailFilter = senders => email => !senders.contains(email.sender)
 
-  val minimumSize: Int => EmailFilter = ???
-  val maximumSize: Int => EmailFilter = ???
+  val minimumSize: Int => EmailFilter = size => email => email.text.length >= size
+  val maximumSize: Int => EmailFilter = size => email => email.text.length <= size
 
   type SizeChecker = Int => Boolean
-  val sizeConstraint: SizeChecker => EmailFilter = ???
+  val sizeConstraint: SizeChecker => EmailFilter = f => email => f(email.text.length)
 
-  val betterMinimumSize: Int => EmailFilter = ???
-  val betterMaximumSize: Int => EmailFilter = ???
+  val betterMinimumSize: Int => EmailFilter = minSize => sizeConstraint(_ >= minSize)
+  val betterMaximumSize: Int => EmailFilter = maxSize => sizeConstraint(_ <= maxSize)
 
-  def complement[A](predicate: A => Boolean) = ???
+  def complement[T](predicate: T => Boolean) = !predicate(_)
 
-  val betterNotSentByAnyOf: Set[String] => EmailFilter = ???
-  val betterBetterMaxSize: Int => EmailFilter = ???
+  val betterNotSentByAnyOf: Set[String] => EmailFilter = strings => complement(sentByOneOf(strings))
+  val betterBetterMaxSize: Int => EmailFilter = betterMinimumSize andThen complement
 
 }
